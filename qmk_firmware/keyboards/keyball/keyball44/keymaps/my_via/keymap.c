f99387558b41b0bcea2b56d928d6fb5178521cad
takumi <takumi50909@gmail.com>
2025-06-29 00:34:27 +0900

    keymap.c更新、KC_TRNS試用

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
@@ -1,21 +1,3 @@
-/*
-Copyright 2022 @Yowkees
-Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
@@ -28,22 +10,20 @@ enum layers {
 enum custom_keycodes {
   TO_JIS = SAFE_RANGE,
   TO_DVORAK,
-  KC_KAKKO  // 「」を入力するためのキー
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-              KC_LALT,KC_LGUI,TO_DVORAK     ,LT(1,KC_SPC),LT(3,KC_LNG1),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , LT(3,KC_SCLN)  , KC_ENT  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
+              KC_LALT,TO_DVORAK,   KC_LGUI,LT(1,KC_SPC),LT(3,KC_LNG1),                  KC_BSPC,LT(2,KC_ENT), _______,                        _______  , KC_ESC
   ),
 
   [1] = LAYOUT_universal(
     SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    SSNP_VRT ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
+    SSNP_VRT ,  KC_LEFT , KC_DOWN  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
     SSNP_HOR ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
                   _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
@@ -57,16 +37,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_universal(
     RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , KC_BTN1  , KC_BTN2  , KC_BTN3 , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+                  QK_BOOT  , KBC_RST  , KC_VOLD  , KC_MPLY  , KC_VOLU  ,                   KC_BTN5  , KC_BTN4  , _______       , KBC_RST  , QK_BOOT
   ),
 
   [4] = LAYOUT_universal(
-  KC_ESC   , KC_QUOT , KC_COMM , KC_DOT  , KC_P    , KC_Y    ,                               KC_F    , KC_G    , KC_C    , KC_R    , KC_L    , KC_DEL   ,
-  KC_TAB   , KC_A    , KC_O    , KC_E    , KC_U    , KC_I    ,                               KC_D    , KC_H    , KC_T    , KC_N    , KC_S    , KC_MINS  ,
-  KC_LSFT  , KC_SCLN , KC_Q    , KC_J    , KC_K    , KC_X    ,                               KC_B    , KC_M    , KC_W    , KC_V    , KC_Z    , KC_INT1  ,
-            KC_LALT, KC_LGUI, TO_JIS   , LT(1,KC_SPC), LT(3,KC_LNG1),             KC_BSPC, LT(2,KC_ENT), TO_JIS , KC_RALT , KC_PSCR
+  KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
+  KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_TRNS  , S(KC_UNDS)  ,
+  KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_MINS  ,
+            KC_LALT, TO_JIS, KC_LGUI   , KC_TRNS, LT(3,KC_LNG1),             KC_BSPC, KC_TRNS, _______, _______ , KC_ESC
 ),
 };
 // clang-format on
@@ -93,23 +73,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case TO_JIS:
             if (record->event.pressed) {
                 default_layer_set(1UL << _JIS);
-                tap_code(KC_LNG1);  // 日本語入力に切替
+                tap_code(KC_LNG1);
             }
             return false;
 
         case TO_DVORAK:
             if (record->event.pressed) {
                 default_layer_set(1UL << _DVORAK);
-                tap_code(KC_LNG2);  // 英語入力に切替
-            }
-            return false;
-
-        case KC_KAKKO:
-            if (record->event.pressed) {
-                // 「」を入力
-                SEND_STRING("「」");
-                // カーソルを「」の間に移動
-                tap_code(KC_LEFT);
+                tap_code(KC_LNG2);
             }
             return false;
     }
