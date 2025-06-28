8993a83ef7f22f019d3a9723604f15576e8cc670
takumi <takumi50909@gmail.com>
2025-06-29 01:29:46 +0900

    keymap.c更新、KC_TRNS試用

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
     _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
     _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
-                  KC_0     , KC_DOT  , KC_KAKKO,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+                  KC_0     , KC_DOT  , _______,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
