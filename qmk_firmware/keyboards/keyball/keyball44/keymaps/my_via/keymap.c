39368e762b7ff70b2c238dfd476937ac28712451
takumi <takumi50909@gmail.com>
2025-07-03 22:39:27 +0900

    changed keymaps for practical use.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,       KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , DV_SLSH   ,
   KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,       KC_H     , KC_J     , KC_K     , KC_L     , LT(7,DV_S)  , DV_MINS  ,
   KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,       KC_N     , KC_M     , DV_W  , DV_V   , DV_Z , DV_TILD  ,
-            KC_LALT, TO_JIS, KC_LGUI   , LT(5,KC_SPC), LT(7,DV_EQL),     KC_ENT, DV_BSLS, _______, _______ , KC_ESC
+            KC_LALT, TO_JIS, KC_LGUI   , LT(5,KC_SPC), LT(7,DV_EQL),     KC_ENT, LT(6,DV_BSLS), _______, _______ , KC_ESC
 ),
 // dvorak > 1
   [5] = LAYOUT_universal(
