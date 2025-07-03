e9722f55e0cf2f74afa327aa46eb443a3ce9e594
takumi <takumi50909@gmail.com>
2025-07-03 22:35:46 +0900

    changed keymaps for practical use.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_universal(
     RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,       _______  ,  KC_LAUNCHPAD  , KC_MISSION_CONTROL  , _______ ,  _______ ,  _______  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,      _______  , KC_BTN1  , KC_BTN2  , KC_BTN3 , _______  , _______  ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,      _______  , KC_BTN1  , KC_BTN3  , KC_BTN2 , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,      CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
              QK_BOOT  , KBC_RST  , KC_VOLD  , KC_MPLY  , KC_VOLU  ,         KC_BTN5  , KC_BTN4  , _______       , _______  , QK_BOOT
   ),
@@ -49,26 +49,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,       KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , DV_SLSH   ,
   KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,       KC_H     , KC_J     , KC_K     , KC_L     , LT(7,DV_S)  , DV_MINS  ,
   KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,       KC_N     , KC_M     , DV_W  , DV_V   , DV_Z , DV_TILD  ,
-            KC_LALT, TO_JIS, KC_LGUI   , LT(5,KC_SPC), LT(7,DV_EQL),     KC_ENT, DV_BSLS, _______, _______ , LT(3,KC_ESC)
+            KC_LALT, TO_JIS, KC_LGUI   , LT(5,KC_SPC), LT(7,DV_EQL),     KC_ENT, DV_BSLS, _______, _______ , KC_ESC
 ),
 // dvorak > 1
   [5] = LAYOUT_universal(
-  KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,       KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , DV_SLSH   ,
-  KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,       KC_H     , KC_J     , KC_K     , KC_L     , LT(3,DV_S)  , DV_MINS  ,
-  KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,       KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_MINS  ,
-            KC_LALT, TO_JIS, KC_LGUI   , LT(1,DV_2), LT(3,DV_EQL),       KC_BSPC, KC_TRNS, _______, _______ , KC_SPC
+  SSNP_FRE ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,       KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
+    SSNP_VRT ,  KC_LEFT , KC_DOWN  , KC_UP   , KC_RGHT  , _______   ,       KC_PGUP  , KC_LEFT  , KC_DOWN    , KC_UP  , KC_RGHT  , KC_F12   ,
+    SSNP_HOR ,  _______ , _______  , _______ , _______  , _______  ,       KC_PGDN  , _______  , _______  , _______  , _______  , _______  ,
+              _______  , _______ , _______  ,  _______  , _______  ,         _______  , _______  , _______       , _______  , _______
 ),
 // dvorak > 2
   [6] = LAYOUT_universal(
-  KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,       KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , DV_SLSH   ,
-  KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,       KC_H     , KC_J     , KC_K     , KC_L     , LT(3,DV_S)  , DV_MINS  ,
-  KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,       KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_MINS  ,
-            KC_LALT, TO_JIS, KC_LGUI   , LT(1,DV_2), LT(3,DV_EQL),      KC_BSPC, KC_TRNS, _______, _______ , KC_ESC
+  _______  , S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),               S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  , _______  ,
+    _______  , KC_1,    KC_2,   KC_3,     KC_4,     KC_5,                 KC_6    , KC_7      , KC_8     , KC_9     , KC_0      , _______,
+    _______  , _______,  _______   , _______   ,_______    ,_______,      _______,_______,_______ ,_______,_______,_______,
+                  KC_0     , KC_DOT  , _______, _______  , _______  ,          KC_DEL   , _______  , _______       , _______  , _______
 ),
 // dvorak > 3
   [7] = LAYOUT_universal(
   RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,       _______  ,  KC_LAUNCHPAD  , KC_MISSION_CONTROL  , _______ ,  _______ ,  _______  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,      _______  , KC_BTN1  , KC_BTN2  , KC_BTN3 , _______  , _______  ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,      _______  , KC_BTN1  , KC_BTN3  , KC_BTN2 , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,      CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
              QK_BOOT  , KBC_RST  , KC_VOLD  , KC_MPLY  , KC_VOLU  ,         KC_BTN5  , KC_BTN4  , _______       , _______  , QK_BOOT
 ),
