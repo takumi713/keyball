abcdb1c18129d0504899304c9f812750561ae7a8
takumi <takumi50909a@gmail.com>
2025-07-04 13:00:40 +0900

    exchange ent and bspc, added right_shift

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
@@ -20,8 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,      KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,      KC_H     , KC_J     , KC_K     , KC_L     , LT(3,KC_SCLN)  , KC_ENT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,      KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
-              KC_LALT,TO_DVORAK,   KC_LGUI,LT(1,KC_SPC),LT(3,KC_BSPC),        LT(2,KC_ENT),KC_RGUI, _______,  _______  , KC_ESC
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,      KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , LT(KC_RSFT,KC_BSLS)  ,
+              KC_LALT,TO_DVORAK,   KC_LGUI,LT(1,KC_SPC),LT(3,KC_ENT),        LT(2,KC_BSPC),KC_RGUI, _______,  _______  , KC_ESC
   ),
 
   [1] = LAYOUT_universal(
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,       _______  ,  KC_LAUNCHPAD  , KC_MISSION_CONTROL  , _______ ,  _______ ,  _______  ,
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , KBC_SAVE  ,       _______  ,  KC_LAUNCHPAD  , KC_MISSION_CONTROL  , _______ ,  _______ ,  _______  ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,      _______  , KC_BTN1  , KC_BTN3  , KC_BTN2 , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,      CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
              QK_BOOT  , KBC_RST  , KC_VOLD  , KC_MPLY  , KC_VOLU  ,         KC_BTN5  , KC_BTN4  , _______       , _______  , QK_BOOT
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [4] = LAYOUT_universal(
   KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,       KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , DV_SLSH   ,
   KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,       KC_H     , KC_J     , KC_K     , KC_L     , LT(7,DV_S)  , DV_MINS  ,
-  KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,       KC_N     , KC_M     , DV_W  , DV_V   , DV_Z , DV_TILD  ,
-            KC_LALT, TO_JIS, KC_LGUI   , LT(5,KC_SPC), LT(7,KC_BSPC),     LT(6,KC_ENT), DV_EQL, _______, _______ , KC_ESC
+  KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,       KC_N     , KC_M     , DV_W  , DV_V   , DV_Z , LT(KC_RSFT,DV_TILD)  ,
+            KC_LALT, TO_JIS, KC_LGUI   , LT(5,KC_SPC), LT(7,KC_ENT),     LT(6,KC_BSPC), KC_RGUI, _______, _______ , KC_ESC
 ),
 // dvorak > 1
   [5] = LAYOUT_universal(
@@ -65,11 +65,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______  , S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),               S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  , _______  ,
     _______  , KC_1,    KC_2,   KC_3,     KC_4,     KC_5,                 KC_6    , KC_7      , KC_8     , KC_9     , KC_0      , DV_BSLS,
     _______  , DV_COLN,  _______   , _______   ,_______    ,_______,      _______, DV_LBRC , DV_RBRC, DV_LCBR, DV_RCBR, DV_GRV,
-                  KC_0     , KC_DOT  , _______, _______  , _______  ,          KC_DEL   , _______  , _______       , _______  , _______
+                  KC_0     , KC_DOT  , DV_PLUS , DV_EQL  , _______  ,          KC_DEL   , _______  , _______       , _______  , _______
 ),
 // dvorak > 3
   [7] = LAYOUT_universal(
-  RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,       _______  ,  KC_LAUNCHPAD  , KC_MISSION_CONTROL  , _______ ,  _______ ,  _______  ,
+  RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , KBC_SAVE  ,       _______  ,  KC_LAUNCHPAD  , KC_MISSION_CONTROL  , _______ ,  _______ ,  _______  ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,      _______  , KC_BTN1  , KC_BTN3  , KC_BTN2 , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,      CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
              QK_BOOT  , KBC_RST  , KC_VOLD  , KC_MPLY  , KC_VOLU  ,         KC_BTN5  , KC_BTN4  , _______       , _______  , QK_BOOT
