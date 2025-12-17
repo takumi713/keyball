3e795713f43ebc0bca718cfc6484eeef2c5f48a8
takumi713 <takumi50909a@gmail.com>
2025-12-18 03:04:43 +0900

    251217 L0k -> LT(1, k)

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
@@ -23,7 +23,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,      KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
-    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,      KC_H     , KC_J     , KC_K     , KC_L     , LT(3,KC_SCLN)  , KC_ENT  ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,      KC_H     , KC_J     , LT(1, KC_K)     , KC_L     , LT(3,KC_SCLN)  , KC_ENT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,      KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,   KC_RSFT, // 「とか使いたくね
               KC_LALT,TO_DVORAK,   KC_LGUI,LT(1,KC_SPC),LT(3,KC_ENT),        LT(2,KC_BSPC),KC_RGUI, _______,  _______  , KC_ESC
   ),
